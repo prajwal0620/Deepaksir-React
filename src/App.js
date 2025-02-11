27211f8e5dc9982f8a74e1847f3a3f8efecdd259
Prajwal <prajwalha27@gmail.com>
2025-02-11 12:57:32 +0530

    react-notes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+ 
+import './App.css'; 
+import Main from './Main';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to React</h1>
+     <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Main(){
+
+    var name = 'Prajwal Hasbe';
+
+    let ott = {
+        webseries: 'Squid Game',
+        platform: 'Netflix'
+    }
+    return(
+        <div> 
+            <h2>
+                Hello from {name}
+            </h2>
+
+            <h3>
+                We are watching {ott.webseries} in {ott.platform}
+            </h3>
+        </div>
+    )
+}
+
+export default Main;
