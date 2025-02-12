8aeb127e5fb6ecbb4f9d076e0263e9d0034ccc62
Prajwal <prajwalha27@gmail.com>
2025-02-12 12:29:04 +0530

    react.notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
  
+import FunctionalComponent from './view/FunctionalComponent.jsx';
+import ClassComponent from './view/ClassComponent.jsx';
+
 import './App.css'; 
-import Main from './Main';
+// import Main from './Main';
+
 
 
 function App() {
   return (
-    <div className="App">
-     <h1>Welcome to React</h1>
-     <Main/>
+    <div>
+     <FunctionalComponent/>
+     <ClassComponent/>
+     
     </div>
   );
 }
